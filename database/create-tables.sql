78f87c2418efe4fa3e685cc1a298b815f8d7c349
Albin Forsberg <foal21fs@student.ju.se>
2023-03-29 13:06:23 +0200

    Added Referee table

---
+++ database/create-tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Person (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    phone varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
+);
